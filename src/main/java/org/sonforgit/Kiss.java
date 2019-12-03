5f2e63056632a67e6aab1ae04480e10b5ab4bf97
VimChen <Vim.W.Chen@pccw.com>
2019-12-03 16:08:22 +0800

    this is a test

--- src/main/java/org/sonforgit/Kiss.java
+++ src/main/java/org/sonforgit/Kiss.java
@@ -3,6 +3,6 @@ package org.sonforgit;
 public class Kiss {
         public void test(){
           /*  this is a git push  test*/  /*  this is a git push  test*/
-                //1122
+                //112233
         }
 }
